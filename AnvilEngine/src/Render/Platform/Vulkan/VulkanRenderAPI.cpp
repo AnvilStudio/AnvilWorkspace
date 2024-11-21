37eeb3c7f74d5ddea31c486b60b089d094ae5b80
Cayden Jordan <seawardsoy1531@gmail.com>
2024-11-20 21:25:10 -0600

    planning

---
+++ AnvilEngine/src/Render/Platform/Vulkan/VulkanRenderAPI.cpp
@@ -0,0 +1,6 @@
+#include "VulkanRenderAPI.h"
+
+anv::VulkanRenderAPI::VulkanRenderAPI(RenderAPICreateInfo _info)
+{
+	ANV_PROFILE_SCOPE();
+}
---
+++ AnvilEngine/src/Render/Platform/Vulkan/VulkanRenderAPI.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Render/RenderAPI.h>
+#include <Render/Renderer.h>
+
+namespace anv
+{
+	class VulkanRenderAPI
+		: public RenderAPI
+	{
+	public:
+		VulkanRenderAPI(RenderAPICreateInfo _info);
+		~VulkanRenderAPI() override {};
+
+	private:
+		Render3DCreateInfo m_RenderInfo;
+	};
+}
+
