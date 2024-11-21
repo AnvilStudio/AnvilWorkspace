56820df844301de34e28373425278e572a1124e7
Cayden Jordan <seawardsoy1531@gmail.com>
2024-11-20 21:22:08 -0600

    Logging and type defs

--- AnvilEngine/src/Util/UMacros.h
+++ AnvilEngine/src/Util/UMacros.h
@@ -2,18 +2,31 @@
 #include "Profile.h"
 #include <memory>
 #include <vector>
+#include "AnvLog/include/AnvLog.h"
 
 namespace anv
 {
+
 // simplification for std::vector
-#define _vec(_ty) std::vector<_ty>  
+template<typename _ty> 
+using _vec = std::vector<_ty>;
 
 // simplification for std::shared_ptr
-#define _shared(_ty) std::shared_ptr<_ty>
+template<typename _ty>
+using _shared = std::shared_ptr<_ty>;
 
 // simplification for std::unique_ptr
-#define _unique(_ty) std::unique_ptr<_ty>
+template<typename _ty>
+using _unique = std::unique_ptr<_ty>;
 
 // profile a specific scope
-#define ANV_PROFILE_SCOPE() Profiler _profile_scope(__FUNCTION__)
+#define ANV_PROFILE_SCOPE() Profiler _profile_scope(__FUNCTION__);
+
+// Logging
+// #define ANV_LOG_INIT(info) anv_log::AnvLog::Init(info);
+#define ANV_LOG_INFO(fmt, ...) anv_log::AnvLog::LOG_INFO (fmt, __VA_ARGS__);
+#define ANV_LOG_DEBUG(fmt, ...) anv_log::AnvLog::LOG_DEBUG(fmt, __VA_ARGS__);
+#define ANV_LOG_WARN(fmt, ...) anv_log::AnvLog::LOG_WARN (fmt, __VA_ARGS__);
+#define ANV_LOG_ERROR(fmt, ...) anv_log::AnvLog::LOG_ERROR(fmt, __VA_ARGS__);
+#define ANV_LOG_FATAL(fmt, ...) anv_log::AnvLog::LOG_FATAL(fmt, __VA_ARGS__);
 }
