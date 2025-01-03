057c7eff5b03fc3da95eb8f744fbccf2ccac1346
Seawardspy1531 <seawardspy1531@gmail.com>
2025-01-02 21:16:13 -0600

    updated for some cross platform

--- AnvilEngine/include/Entry.h
+++ AnvilEngine/include/Entry.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <iostream>
+#include "../src/Core/Macros.h"
 #include "Anvil.h"   
 
 extern anv::App* CreateApp();
 
+#ifdef PLATFORM_WIN64
 #ifdef DEBUG
 
 int main(int arg_c, char* arg_v[])
@@ -32,3 +34,13 @@ int WinMain()
     delete app;
 }
 #endif // RELEASE
+#else // Platform ?
+int main(int arg_c, char* arg_v[])
+{
+    anv::App* app = CreateApp();
+
+    app->Run();
+
+    delete app;
+}
+#endif
