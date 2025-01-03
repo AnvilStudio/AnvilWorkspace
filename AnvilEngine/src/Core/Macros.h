44dfe8151eb4a5f4206c0ad07a7512e99b82e532
Seawardspy1531 <seawardspy1531@gmail.com>
2025-01-02 21:19:26 -0600

    better detection

--- AnvilEngine/src/Core/Macros.h
+++ AnvilEngine/src/Core/Macros.h
@@ -1,5 +1,21 @@
+////////////////////////////////////////////////////////////////
+//                                                            //
+// Macros.h is used mainly for Compiler/OS and hardware       //
+// specific detection.                                        //
+//                                                            //
+////////////////////////////////////////////////////////////////
+
 #pragma once
 
-#ifdef _WIN32
-# define PLATFORM_WIN32 1
-#endif
+/// OS detection
+# ifdef _WIN32
+	# ifdef _WIN64
+		# define PLATFORM_WIN64 1 // Windows 64 bit 
+	#else
+		# error "32bit applications are not supported!"
+		# define PLATFORM_WIN64 // Windows 32 bit
+	# endif
+# endif
+
+
+// TODO (Alba): Impl Apple platform detection. (Follow a similar format of windows)
