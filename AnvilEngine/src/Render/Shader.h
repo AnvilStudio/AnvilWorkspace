dcd29dbcb1805c62eadcacd372000bfe49733639
cj0x7c00 <caydenjordan05@gmail.com>
2025-01-16 15:11:34 -0600

    updated

--- AnvilEngine/src/Render/Shader.h
+++ AnvilEngine/src/Render/Shader.h
@@ -12,16 +12,23 @@ namespace anv
 		NONE
 	};
 
+	// Extends Asset for serialization
 	class Shader : public Asset
 	{
 	public:
 		static Ref<Shader> Create(const std::string& _shaderPath, _shared<Context> _ctx);
 
 		template<typename T>
-		inline T* GetAs()
+		inline T* GetAs() const
 		{
 			return dynamic_cast<T*>(this);
 		}
+
+	private:
+
+		// Write the shaders compiled src 
+		// to either a json fmt or bin fmt.
+		void OnSerialize() override {};
 
 	private:
 		static inline std::unordered_map<std::string, Ref<Shader>> s_ShaderCache;
