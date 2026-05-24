d7cdcc8b9c7ef96050b4ea60b0c02c1946a9effe
cj0x7c00 <caydenjordan05@gmail.com>
2026-05-24 15:26:07 -0500

    Update scene and shaders for alpha support
    Removed `Environment` and `Main Camera` entities from `NewScene.ascn` while keeping the scene UUID unchanged.
    
    Updated `sprite.glsl` to support alpha transparency:
    - Changed `inColor` and `fragColor` from `vec3` to `vec4` in the vertex shader.
    - Simplified `outColor` assignment in the fragment shader to directly use the updated `fragColor`.

--- Forge/TestPrj/Assets/com.anvstu.engine/ShaderLib/sprite.glsl
+++ Forge/TestPrj/Assets/com.anvstu.engine/ShaderLib/sprite.glsl
@@ -2,9 +2,9 @@
 #version 450
 
 layout(location = 0) in vec2 inPosition;
-layout(location = 1) in vec3 inColor;
+layout(location = 1) in vec4 inColor;
 
-layout(location = 0) out vec3 fragColor;
+layout(location = 0) out vec4 fragColor;
 
 void main()
 {
@@ -16,8 +16,8 @@ void main()
 #version 450
 
 layout(location = 0) out vec4 outColor;
-layout(location = 0) in vec3 fragColor;
+layout(location = 0) in vec4 fragColor;
 
 void main() {
-    outColor = vec4(fragColor, 1.0);
+    outColor = fragColor;
 }
