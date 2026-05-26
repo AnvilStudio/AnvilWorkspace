94c0381dce7b3d2a538379dce3ec5a450834adf6
cj0x7c00 <caydenjordan05@gmail.com>
2026-05-25 20:15:26 -0500

    updated shader

--- ProjectCreator/project_creator.py
+++ ProjectCreator/project_creator.py
@@ -51,8 +51,12 @@ Path = '@Assets/Scenes/Default.ascn'
     write(engine / "ShaderLib" / "sprite.glsl", """#type vert
 #version 450
 
-layout(location = 0) in vec2 inPosition;
-layout(location = 1) in vec4 inColor;
+layout(push_constant)
+uniform PushData
+{
+    mat4 model;
+    vec4 color;
+} push_data;
 
 layout(set = 0, binding = 0) uniform Camera
 {
@@ -61,22 +65,27 @@ layout(set = 0, binding = 0) uniform Camera
     mat4 ViewProjection;
 } camera;
 
+layout(location = 0) in vec2 inPosition;
+layout(location = 1) in vec4 inColor;
+
 layout(location = 0) out vec4 fragColor;
 
 void main()
 {
-    fragColor = inColor;
-    gl_Position = camera.ViewProjection * vec4(inPosition, 0.0, 1.0);
-}
+	gl_Position = camera.ViewProjection * 
+    push_data.model * 
+    vec4(inPosition, 0.0, 1.0);
 
+	fragColor = push_data.color;
+}
+ 
 #type frag
 #version 450
 
-layout(location = 0) in vec4 fragColor;
 layout(location = 0) out vec4 outColor;
+layout(location = 0) in vec4 fragColor;
 
-void main()
-{
+void main() {
     outColor = fragColor;
 }
 """)
