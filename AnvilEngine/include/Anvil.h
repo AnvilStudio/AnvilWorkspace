02f49b81d49a63d66d3807ce1d9bbdf450951f8d
cj0x7c00 <caydenjordan05@gmail.com>
2026-06-01 15:57:17 -0500

    Expand dependencies in Anvil.h for new functionality
    Added include directives for `imgui.h`, `Renderer.h`, `Component.h`, and `Scene.h` to integrate ImGui, rendering, and scene management features. These changes enhance the capabilities of the `Anvil.h` file by incorporating new functionality from the respective modules.

--- AnvilEngine/include/Anvil.h
+++ AnvilEngine/include/Anvil.h
@@ -1,4 +1,8 @@
 #pragma once
 
 #include "../src/Core/App.h"
+#include "../vendor/imgui/imgui.h"
 #include "../vendor/glm/glm/glm.hpp"
+#include "../src/Render/Renderer.h"
+#include "../src/Scene/Component.h"
+#include "../src/Scene/Scene.h"
