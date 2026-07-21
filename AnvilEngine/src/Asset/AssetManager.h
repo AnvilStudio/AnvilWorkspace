200c9a946e5499cf7984775f7b12dafbc3e079bf
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-21 18:24:47 -0400

    Add resource lookup and reuse to asset manager

--- AnvilEngine/src/Asset/AssetManager.h
+++ AnvilEngine/src/Asset/AssetManager.h
@@ -18,12 +18,20 @@ namespace anv
 
         // -------- GET (never creates) --------
         Ref<Asset> Get(const uuid::AssetUUID& _id) const;
+        Ref<Asset> GetByResource(const std::filesystem::path& _resource) const;
 
         template<class TAsset>
         Ref<TAsset> GetAs(const uuid::AssetUUID& _id) const
         {
             static_assert(std::is_base_of_v<Asset, TAsset>);
             return Get(_id).Cast<TAsset>();
+        }
+
+        template<class TAsset>
+        Ref<TAsset> GetByResourceAs(const std::filesystem::path& _resource) const
+        {
+            static_assert(std::is_base_of_v<Asset, TAsset>);
+            return GetByResource(_resource).Cast<TAsset>();
         }
 
         // -------- CREATE (never searches) --------
@@ -38,20 +46,35 @@ namespace anv
             asset->GenMetaFile();
 
             m_AssetReg.try_emplace(id, asset.template As<Asset>());
+
+            if (!asset->GetResourcePath().empty())
+                m_ByResource.insert_or_assign(NormalizeResource(asset->GetResourcePath()), id);
+
             return asset;
+        }
+
+        template<class TAsset, class... Args>
+        Ref<TAsset> GetOrCreate(const std::filesystem::path& _resource, Args&&... _args)
+        {
+            static_assert(std::is_base_of_v<Asset, TAsset>);
+
+            if (auto existing = GetByResourceAs<TAsset>(_resource))
+                return existing;
+
+            return Create<TAsset>(_resource, std::forward<Args>(_args)...);
         }
 
         Ref<GraphicsPipeline> CreateGraphicsPipeline(_shared<Context> _ctx, std::string _dName);
         Ref<Shader> CreateShader(const std::string& _shaderPath, _shared<Context> _ctx);
 
     private:
+        static std::string NormalizeResource(const std::filesystem::path& _resource);
         void resolve_assets();
         void create(Deserialized& _dser);
 
     private:
         FileSystem& m_Fs;
         std::unordered_map<uuid::AssetUUID, Ref<Asset>> m_AssetReg;
-        std::unordered_map<std::string, uuid::AssetUUID> m_ByResource; // de-dupe
+        std::unordered_map<std::string, uuid::AssetUUID> m_ByResource;
     };
 }
-
