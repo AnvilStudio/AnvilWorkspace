e21ef5e1d15f09c2464c5ca37200271bf8252637
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-21 21:31:24 -0400

    resolve sprite texture assets during scene rendering

--- AnvilEngine/src/Scene/2D/SceneRenderer2D.cpp
+++ AnvilEngine/src/Scene/2D/SceneRenderer2D.cpp
@@ -2,14 +2,16 @@
 #include "../Component.h"
 #include "../vendor/entt/single_include/entt/entt.hpp"
 #include <Render/Renderer.h>
+#include <Asset/AssetTypes/Texture.h>
+#include <Core/App.h>
 #include <algorithm>
 #include <vector>
 
 namespace anv
 {
-	void SceneRenderer2D::Render(Ref<Scene> _scene)
-	{
-        auto view = _scene->m_Registry.view<
+    void SceneRenderer2D::Render(Ref<Scene> scene)
+    {
+        auto view = scene->m_Registry.view<
             Component::Transform2d,
             Component::SpriteRenderer>();
 
@@ -26,21 +28,23 @@ namespace anv
             {
                 return view.get<Component::SpriteRenderer>(left).drawLayer <
                     view.get<Component::SpriteRenderer>(right).drawLayer;
-            }
-        );
+            });
+
+        auto assetManager = App::GetInstance()->GetAssetManager();
 
         for (auto entity : drawOrder)
         {
             auto& transform = view.get<Component::Transform2d>(entity);
             auto& sprite = view.get<Component::SpriteRenderer>(entity);
+            Ref<Texture> texture = assetManager->GetAs<Texture>(sprite.texture);
 
             Renderer2D::DrawQuad(
                 transform.position,
                 transform.rotation,
                 transform.scale,
                 sprite.color,
-                sprite.drawLayer
-            );
+                texture,
+                sprite.drawLayer);
         }
-	}
+    }
 }
