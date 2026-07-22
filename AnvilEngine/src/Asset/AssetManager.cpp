a8c21b3d81a51c736fae8929321c9a82efc3301b
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-21 21:29:15 -0400

    register backend texture assets through AssetManager

--- AnvilEngine/src/Asset/AssetManager.cpp
+++ AnvilEngine/src/Asset/AssetManager.cpp
@@ -8,121 +8,147 @@
 
 namespace anv
 {
-	AssetManager::AssetManager()
-		: m_Fs(App::GetInstance()->GetFS())
-	{
-		m_Fs.MountKey("AssetMeta", "Assets/com.anvstu.engine/AssetMeta/");
-		resolve_assets();
-	}
+    AssetManager::AssetManager()
+        : m_Fs(App::GetInstance()->GetFS())
+    {
+        m_Fs.MountKey("AssetMeta", "Assets/com.anvstu.engine/AssetMeta/");
+        resolve_assets();
+    }
 
-	AssetManager::~AssetManager()
-	{
-		// Ensure all assets are saved
-		for (auto a : m_AssetReg)
-		{
-			a.second->Save();
-		}
-	}
+    AssetManager::~AssetManager()
+    {
+        for (auto& [id, asset] : m_AssetReg)
+            asset->Save();
+    }
 
-	std::string AssetManager::NormalizeResource(const std::filesystem::path& _resource)
-	{
-		std::error_code error;
-		auto normalized = std::filesystem::weakly_canonical(_resource, error);
+    std::string AssetManager::NormalizeResource(const std::filesystem::path& resource)
+    {
+        std::error_code error;
+        auto normalized = std::filesystem::weakly_canonical(resource, error);
 
-		if (error)
-		{
-			error.clear();
-			normalized = std::filesystem::absolute(_resource, error);
-		}
+        if (error)
+        {
+            error.clear();
+            normalized = std::filesystem::absolute(resource, error);
+        }
 
-		if (error)
-			normalized = _resource.lexically_normal();
+        if (error)
+            normalized = resource.lexically_normal();
 
-		return normalized.generic_string();
-	}
+        return normalized.generic_string();
+    }
 
-	Ref<Asset> AssetManager::Get(const uuid::AssetUUID& _id) const
-	{
-		auto it = m_AssetReg.find(_id);
-		if (it == m_AssetReg.end())
-			return nullptr;
-		return it->second;
-	}
+    void AssetManager::Register(Ref<Asset> asset)
+    {
+        if (!asset)
+            return;
 
-	Ref<Asset> AssetManager::GetByResource(const std::filesystem::path& _resource) const
-	{
-		auto resourceIt = m_ByResource.find(NormalizeResource(_resource));
-		if (resourceIt == m_ByResource.end())
-			return nullptr;
+        const uuid::AssetUUID id = asset->GetAssetID();
+        asset->GenMetaFile();
+        m_AssetReg.insert_or_assign(id, asset);
 
-		return Get(resourceIt->second);
-	}
+        if (!asset->GetResourcePath().empty())
+        {
+            m_ByResource.insert_or_assign(
+                NormalizeResource(asset->GetResourcePath()),
+                id);
+        }
+    }
 
-	void AssetManager::create(Deserialized& _dser)
-	{
-		if (_dser.type == "Shader")
-		{
-			auto shader = Shader::Create(_dser);
-			shader->GenMetaFile();
-			m_AssetReg.try_emplace(shader->GetAssetID(), shader);
+    Ref<Asset> AssetManager::Get(const uuid::AssetUUID& id) const
+    {
+        auto it = m_AssetReg.find(id);
+        return it == m_AssetReg.end() ? nullptr : it->second;
+    }
 
-			if (!shader->GetResourcePath().empty())
-				m_ByResource.insert_or_assign(
-					NormalizeResource(shader->GetResourcePath()),
-					shader->GetAssetID());
-		}
+    Ref<Asset> AssetManager::GetByResource(const std::filesystem::path& resource) const
+    {
+        auto resourceIt = m_ByResource.find(NormalizeResource(resource));
+        return resourceIt == m_ByResource.end() ? nullptr : Get(resourceIt->second);
+    }
 
-		if (_dser.type == "Texture")
-		{
-			ANV_LOG_DEBUG("Texture");
-			Create<Texture>(_dser);
-		}
-	}
+    Ref<Texture> AssetManager::CreateTexture(const std::filesystem::path& resource)
+    {
+        auto texture = Texture::Create(resource);
+        Register(texture.As<Asset>());
+        return texture;
+    }
 
-	Ref<GraphicsPipeline> AssetManager::CreateGraphicsPipeline(_shared<Context> _ctx, std::string _dName)
-	{
-		auto p = GraphicsPipeline::create_pipeline_asset(_ctx, _dName);
+    Ref<Texture> AssetManager::CreateTexture(Deserialized& deserialized)
+    {
+        auto texture = Texture::Create(deserialized);
+        Register(texture.As<Asset>());
+        return texture;
+    }
 
-		const uuid::AssetUUID id = p->GetAssetID();
-		p->GenMetaFile();
-		m_AssetReg.try_emplace(id, p.As<Asset>());
-		return p;
-	}
+    Ref<Texture> AssetManager::GetOrCreateTexture(const std::filesystem::path& resource)
+    {
+        if (auto existing = GetByResourceAs<Texture>(resource))
+            return existing;
 
-	Ref<Shader> AssetManager::CreateShader(const std::string& _shaderPath, _shared<Context> _ctx)
-	{
-		if (auto existing = GetByResourceAs<Shader>(_shaderPath))
-			return existing;
+        return CreateTexture(resource);
+    }
 
-		auto s = Shader::Create(_shaderPath, _ctx);
+    void AssetManager::create(Deserialized& deserialized)
+    {
+        if (deserialized.type == "Shader")
+        {
+            auto shader = Shader::Create(deserialized);
+            Register(shader.As<Asset>());
+        }
+        else if (deserialized.type == "Texture")
+        {
+            CreateTexture(deserialized);
+        }
+    }
 
-		const uuid::AssetUUID id = s->GetAssetID();
-		s->GenMetaFile();
-		m_AssetReg.try_emplace(id, s.As<Asset>());
-		m_ByResource.insert_or_assign(NormalizeResource(s->GetResourcePath()), id);
-		return s;
-	}
+    Ref<GraphicsPipeline> AssetManager::CreateGraphicsPipeline(
+        _shared<Context> context,
+        std::string displayName)
+    {
+        auto pipeline = GraphicsPipeline::create_pipeline_asset(context, displayName);
+        Register(pipeline.As<Asset>());
+        return pipeline;
+    }
 
-	void AssetManager::resolve_assets()
-	{
-		auto assets = m_Fs.GetKeyVal("AssetMeta");
+    Ref<Shader> AssetManager::CreateShader(
+        const std::string& shaderPath,
+        _shared<Context> context)
+    {
+        if (auto existing = GetByResourceAs<Shader>(shaderPath))
+            return existing;
 
-		m_Fs.ForEach(assets, [&](Ref<File> _file)
-		{
-				Serializer ser(_file, Serializer::Mode::SER_MODE_TOML, Serializer::Direction::Read);
-				Deserialized dser;
-				dser.ser = ser;
+        auto shader = Shader::Create(shaderPath, context);
+        Register(shader.As<Asset>());
+        return shader;
+    }
 
-				ser.ObjectStrict("Asset", [&] {
-					ser.FieldStrict("Name", dser.name);
-					ser.FieldStrict("Resource", dser.resource);
-					ser.FieldStrict("UUID", dser.uuid);
-					ser.ObjectStrict("Spec", [&] {
-					ser.FieldStrict("Type", dser.type);
-						});
-					});
+    void AssetManager::resolve_assets()
+    {
+        auto assets = m_Fs.GetKeyVal("AssetMeta");
 
-				create(dser);
-		});
-	}
+        m_Fs.ForEach(assets, [&](Ref<File> file)
+        {
+            Serializer serializer(
+                file,
+                Serializer::Mode::SER_MODE_TOML,
+                Serializer::Direction::Read);
+
+            Deserialized deserialized;
+            deserialized.ser = serializer;
+
+            serializer.ObjectStrict("Asset", [&]
+            {
+                serializer.FieldStrict("Name", deserialized.name);
+                serializer.FieldStrict("Resource", deserialized.resource);
+                serializer.FieldStrict("UUID", deserialized.uuid);
+                serializer.ObjectStrict("Spec", [&]
+                {
+                    serializer.FieldStrict("Type", deserialized.type);
+                });
+            });
+
+            create(deserialized);
+        });
+    }
 }
