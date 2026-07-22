02c902d70fcbbbc7c63c1f149faee997b7cf4983
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-21 21:30:00 -0400

    carry texture assets in sprite submissions

--- AnvilEngine/src/Render/RenderData.h
+++ AnvilEngine/src/Render/RenderData.h
@@ -1,21 +1,25 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <Core/Reference.h>
 
 namespace anv
 {
-	struct QuadVertex
-	{
-		glm::vec2 Position;
-	};
-	
+    class Texture;
+
+    struct QuadVertex
+    {
+        glm::vec2 Position;
+        glm::vec2 TexCoord;
+    };
+
     struct Quad
     {
         static inline QuadVertex vertices[4] =
         {
-            {{-0.5f,-0.5f}},
-            {{ 0.5f,-0.5f}},
-            {{ 0.5f, 0.5f}},
-            {{-0.5f, 0.5f}}
+            {{-0.5f, -0.5f}, {0.0f, 0.0f}},
+            {{ 0.5f, -0.5f}, {1.0f, 0.0f}},
+            {{ 0.5f,  0.5f}, {1.0f, 1.0f}},
+            {{-0.5f,  0.5f}, {0.0f, 1.0f}}
         };
 
         static inline uint32_t indices[6] =
@@ -25,13 +29,13 @@ namespace anv
         };
     };
 
-	struct QuadSubmission
-	{
-		glm::vec2 Position;
-        float Rotaion;
-		glm::vec2 Size;
-		glm::vec4 Color;
-
-        int Layer;
-	};
+    struct QuadSubmission
+    {
+        glm::vec2 Position{};
+        float Rotation = 0.0f;
+        glm::vec2 Size{1.0f};
+        glm::vec4 Color{1.0f};
+        Ref<Texture> TextureAsset = nullptr;
+        int Layer = 0;
+    };
 }
