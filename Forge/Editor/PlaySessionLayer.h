1c9218a83ed09a5e85f265f447b7c94d1ff9b67a
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-24 00:28:37 -0400

    Add play session snapshot layer

---
+++ Forge/Editor/PlaySessionLayer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Anvil.h>
+
+#include <filesystem>
+
+class EditorLayer;
+
+/**
+ * @brief Snapshots the edit scene when Play begins and restores it on Stop.
+ */
+class PlaySessionLayer : public anv::Layer
+{
+public:
+    explicit PlaySessionLayer(EditorLayer* _editorLayer);
+
+    void OnImGuiRender() override;
+    void OnDetach() override;
+
+private:
+    void begin_play_session();
+    void end_play_session();
+    void discard_snapshot();
+
+private:
+    EditorLayer* m_EditorLayer = nullptr;
+    bool m_WasPlaying = false;
+    std::filesystem::path m_OriginalScenePath;
+    std::filesystem::path m_SnapshotPath;
+};
