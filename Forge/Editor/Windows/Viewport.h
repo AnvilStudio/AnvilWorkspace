dc5cebccca7fc8104ee9a9006a99ed828845388d
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-31 00:22:34 -0400

    Add viewport selection overlay interface

--- Forge/Editor/Windows/Viewport.h
+++ Forge/Editor/Windows/Viewport.h
@@ -11,7 +11,13 @@ private:
     void update_operation(bool update);
     void set_gizmo_bounds();
     void draw_gizmo();
+    void draw_selection_overlay(const anv::Ref<anv::Scene>& scene);
     void pick_entity(const anv::Ref<anv::Scene>& scene);
+
+    bool world_to_viewport(
+        const glm::vec3& worldPosition,
+        ImVec2& screenPosition
+    ) const;
 
 private:
     anv::Ref<anv::RenderTarget> m_ViewportTarget;
@@ -21,8 +27,8 @@ private:
     uint32_t m_LastTargetWidth = 0;
     uint32_t m_LastTargetHeight = 0;
 
-    glm::vec2 m_ViewportBounds[2];
-    glm::vec2 m_ViewportSize;
+    glm::vec2 m_ViewportBounds[2]{};
+    glm::vec2 m_ViewportSize{0.0f};
     bool m_ViewportHovered = false;
     bool m_ViewportFocused = false;
 
