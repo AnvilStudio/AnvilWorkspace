cab7773768b8d7b9f6eeaf96435fab89a183daf6
Cj0x7c00 <caydenjordan05@gmail.com>
2026-07-31 15:56:08 -0400

    fixed typo

--- AnvilEngine/src/Scene/Manager.cpp
+++ AnvilEngine/src/Scene/Manager.cpp
@@ -177,7 +177,7 @@ namespace anv
             scene->Save();
 
         ANV_LOG_INFO("Opened scene '%s'.", scenePath.string().c_str());
-        Renderer2D::SetCamera(scene->GetMainCamera());
+        Renderer2D::SetCamera(scene->GetActiveCamera());
         return scene;
     }
 
