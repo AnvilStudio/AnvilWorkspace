be8d2e87c14a7e0c01752c624b9416d1883e6489
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-31 02:22:52 -0400

    Remove duplicate game viewport runtime lifecycle state

--- Forge/Editor/Windows/GameViewport.h
+++ Forge/Editor/Windows/GameViewport.h
@@ -9,14 +9,10 @@ public:
     void Draw();
 
 private:
-    void synchronize_scene_runtime();
-
-private:
     anv::Ref<anv::RenderTarget> m_GameViewportTarget;
 
     bool m_OnPlayFocused = false;
     bool m_InputEnabled = false;
-    bool m_WasPlaying = false;
 
     uint32_t m_LastTargetWidth = 0;
     uint32_t m_LastTargetHeight = 0;
