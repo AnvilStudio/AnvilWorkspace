5e935a1ea48d542ad52438fe43de7752ffd7ebe3
Cayden Jordan <seawardspy1531@gmail.com>
2026-07-30 23:41:25 -0400

    Fix scene hierarchy rename state

--- Forge/Editor/Windows/SceneHierarchy.h
+++ Forge/Editor/Windows/SceneHierarchy.h
@@ -1,27 +1,40 @@
 #pragma once
+
 #include <Anvil.h>
+
+#include <array>
+#include <string>
 
 using namespace anv;
 
 class SceneHierarchy
 {
 public:
-    SceneHierarchy(Ref<Scene> scene);
+    explicit SceneHierarchy(Ref<Scene> scene);
 
     void Draw();
 
 private:
-    void handle_rename(entt::entity entity, 
-    entt::entity& selected_entity,
-    Component::Tag& tag, 
-    std::string label, 
-    bool selected);
+    void begin_rename(
+        entt::entity entity,
+        const std::string& currentName
+    );
+
+    void handle_rename(
+        entt::entity entity,
+        Component::Tag& tag,
+        bool selected
+    );
+
+    void finish_rename(Component::Tag& tag);
+    void cancel_rename();
 
 private:
-    Ref<Scene> m_ActiveScene;
-    entt::entity m_EntityToDelete;
+    entt::entity m_EntityToDelete = entt::null;
+    entt::entity m_RenamedEntity = entt::null;
 
-    bool m_IsRenaming;
-    entt::entity m_RenamedEntity;
-    _vec<char> m_RenameBuffer{};
+    bool m_IsRenaming = false;
+    bool m_FocusRenameInput = false;
+
+    std::array<char, 256> m_RenameBuffer{};
 };
