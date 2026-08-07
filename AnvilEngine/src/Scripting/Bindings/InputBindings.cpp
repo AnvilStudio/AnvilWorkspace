c223ca46179e8f5205abb0e793e1f6570f342ca3
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-06 23:35:36 -0400

    Extract Python input bindings

---
+++ AnvilEngine/src/Scripting/Bindings/InputBindings.cpp
@@ -0,0 +1,35 @@
+#include "PythonBindings.h"
+
+#ifdef ANV_ENABLE_PYTHON
+#include "../../Core/App.h"
+
+namespace anv::python
+{
+    PyObject* IsKeyPressed(PyObject*, PyObject* args)
+    {
+        int key = 0;
+        if (!PyArg_ParseTuple(args, "i", &key))
+            return nullptr;
+
+        App* app = App::GetInstance();
+        if (!app)
+        {
+            PyErr_SetString(
+                PyExc_RuntimeError,
+                "The Anvil application instance is unavailable.");
+            return nullptr;
+        }
+
+        auto inputSystem = app->GetInputSystem();
+        if (!inputSystem)
+        {
+            PyErr_SetString(
+                PyExc_RuntimeError,
+                "The Anvil input system is unavailable.");
+            return nullptr;
+        }
+
+        return PyBool_FromLong(inputSystem->IsKeyPressed(key) ? 1 : 0);
+    }
+}
+#endif
