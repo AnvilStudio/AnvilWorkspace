8d9507fcd3aaa2a9aa15991bc5194c3e1553951b
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 20:56:50 -0400

    Export Python component wrapper types

--- AnvilEngine/Resources/Python/anvil/__init__.py
+++ AnvilEngine/Resources/Python/anvil/__init__.py
@@ -3,11 +3,15 @@ from _anvil import log, warn, error
 from .script import Script
 from .input import input
 from .key import Key
+from .transform import Transform2D
 from .physics import RigidBody2D
+from .collider import BoxCollider2D
 
 __all__ = [
     "Script",
+    "Transform2D",
     "RigidBody2D",
+    "BoxCollider2D",
     "Key",
     "input",
     "log",
