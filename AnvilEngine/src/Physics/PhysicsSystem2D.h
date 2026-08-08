d41c5f91fbfa9c6a58a2c8d7d4fbe8e3ef67121a
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 22:31:41 -0400

    Make PhysicsSystem2D the concrete physics system type

--- AnvilEngine/src/Physics/PhysicsSystem2D.h
+++ AnvilEngine/src/Physics/PhysicsSystem2D.h
@@ -1,12 +1,67 @@
 #pragma once
 
-#include "Physics2D.h"
+#include "PhysicsBody2D.h"
+#include "PhysicsWorld2D.h"
+#include "../Core/Reference.h"
+#include "../vendor/entt/single_include/entt/entt.hpp"
+
+#include <box2d/box2d.h>
+
+#include <unordered_map>
 
 namespace anv
 {
-    // Transitional architecture alias.
-    // PhysicsSystem2D is the scene-facing physics system name going forward.
-    // The underlying Physics2D implementation will be split into
-    // PhysicsSystem2D, PhysicsWorld2D, and PhysicsBody2D incrementally.
-    using PhysicsSystem2D = Physics2D;
+    class Scene;
+
+    class PhysicsSystem2D
+    {
+    public:
+        PhysicsSystem2D() = default;
+        ~PhysicsSystem2D();
+
+        PhysicsSystem2D(const PhysicsSystem2D&) = delete;
+        PhysicsSystem2D& operator=(const PhysicsSystem2D&) = delete;
+
+        bool Start(Scene& scene);
+        void Stop();
+        void Step(Scene& scene, float deltaTime);
+
+        void DestroyBody(entt::entity entity);
+        bool IsRunning() const { return m_Running; }
+
+        bool AddForce(entt::entity entity, float x, float y)
+        {
+            const auto it = m_Bodies.find(entity);
+            if (!m_Running || it == m_Bodies.end())
+                return false;
+
+            return it->second.AddForce(x, y);
+        }
+
+        bool ApplyImpulse(entt::entity entity, float x, float y)
+        {
+            const auto it = m_Bodies.find(entity);
+            if (!m_Running || it == m_Bodies.end())
+                return false;
+
+            return it->second.ApplyImpulse(x, y);
+        }
+
+    private:
+        void CreateBody(Scene& scene, entt::entity entity);
+        void RemoveDestroyedBodies(Scene& scene);
+        void SynchronizeBodiesFromTransforms(Scene& scene);
+        void SynchronizeTransforms(Scene& scene);
+
+        static b2BodyType ToBox2DType(int type);
+
+    private:
+        PhysicsWorld2D m_World;
+        std::unordered_map<entt::entity, PhysicsBody2D> m_Bodies;
+
+        float m_Accumulator = 0.0f;
+        float m_FixedTimeStep = 1.0f / 60.0f;
+        int m_SubStepCount = 4;
+        bool m_Running = false;
+    };
 }
