f471d7dd080f11d538497ecd2979fe6730112c93
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 20:05:24 -0400

    Add script entity context

---
+++ AnvilEngine/src/Scripting/ScriptEntityContext.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <entt/entt.hpp>
+
+#include <string_view>
+
+namespace anv
+{
+    class Scene;
+
+    struct ScriptEntityContext
+    {
+        Scene* scene = nullptr;
+        entt::entity entity = entt::null;
+
+        explicit operator bool() const
+        {
+            return scene != nullptr && entity != entt::null;
+        }
+    };
+
+    void RegisterScriptScene(Scene& scene);
+    void UnregisterScriptScene(Scene& scene);
+    void ClearScriptScenes();
+
+    ScriptEntityContext ResolveScriptEntity(std::string_view entityID);
+}
