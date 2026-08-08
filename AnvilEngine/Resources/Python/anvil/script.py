0ad7b86df0a5ca220d82a6de9ea1c1e91c132519
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 20:56:41 -0400

    Add generic script component access

--- AnvilEngine/Resources/Python/anvil/script.py
+++ AnvilEngine/Resources/Python/anvil/script.py
@@ -1,44 +1,66 @@
-from _anvil import (
-    _get_position,
-    _set_position,
-    _get_rotation,
-    _set_rotation,
-)
+import _anvil
 
+from .transform import Transform2D
 from .physics import RigidBody2D
+from .collider import BoxCollider2D
 
 
 class Script:
     def __init__(self):
         self.entity_id = ""
 
-    @property
-    def position(self):
-        return _get_position(self.entity_id)
+    def has_component(self, component_type) -> bool:
+        component_name = getattr(component_type, "_component_name", None)
+        if component_name is None:
+            raise TypeError("component_type must be an Anvil component type")
 
-    @position.setter
-    def position(self, value):
-        x, y = value
-        _set_position(
-            self.entity_id,
-            float(x),
-            float(y)
+        return bool(
+            _anvil._has_component(
+                self.entity_id,
+                component_name
+            )
         )
 
-    @property
-    def rotation(self):
-        return _get_rotation(self.entity_id)
+    def get_component(self, component_type):
+        component_name = getattr(component_type, "_component_name", None)
+        if component_name is None:
+            raise TypeError("component_type must be an Anvil component type")
 
-    @rotation.setter
-    def rotation(self, value):
-        _set_rotation(
-            self.entity_id,
-            float(value)
-        )
+        if not self.has_component(component_type):
+            raise KeyError(
+                f"Entity does not have component {component_name}"
+            )
+
+        return component_type(self.entity_id)
+
+    @property
+    def transform(self):
+        return self.get_component(Transform2D)
 
     @property
     def rigid_body(self):
-        return RigidBody2D(self.entity_id)
+        return self.get_component(RigidBody2D)
+
+    @property
+    def box_collider(self):
+        return self.get_component(BoxCollider2D)
+
+    # Compatibility aliases. Prefer self.transform.position/rotation.
+    @property
+    def position(self):
+        return self.transform.position
+
+    @position.setter
+    def position(self, value):
+        self.transform.position = value
+
+    @property
+    def rotation(self):
+        return self.transform.rotation
+
+    @rotation.setter
+    def rotation(self, value):
+        self.transform.rotation = value
 
     def on_create(self):
         pass
