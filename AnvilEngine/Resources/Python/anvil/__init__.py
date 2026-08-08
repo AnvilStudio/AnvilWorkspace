d8331f91ea1586c2b04ef7446aef8089fb0fa07b
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 20:12:26 -0400

    Export Python rigid body API

--- AnvilEngine/Resources/Python/anvil/__init__.py
+++ AnvilEngine/Resources/Python/anvil/__init__.py
@@ -3,9 +3,11 @@ from _anvil import log, warn, error
 from .script import Script
 from .input import input
 from .key import Key
+from .physics import RigidBody2D
 
 __all__ = [
     "Script",
+    "RigidBody2D",
     "Key",
     "input",
     "log",
