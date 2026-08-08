163dc97054ca5d3d4c5055e1e976aae0f35ce362
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-07 22:52:45 -0400

    Add single key press Python binding

--- AnvilEngine/src/Scripting/Bindings/InputBindings.cpp
+++ AnvilEngine/src/Scripting/Bindings/InputBindings.cpp
@@ -5,31 +5,56 @@
 
 namespace anv::python
 {
+    namespace
+    {
+        _shared<InputSystem> ResolveInputSystem()
+        {
+            App* app = App::GetInstance();
+            if (!app)
+            {
+                PyErr_SetString(
+                    PyExc_RuntimeError,
+                    "The Anvil application instance is unavailable.");
+                return nullptr;
+            }
+
+            auto inputSystem = app->GetInputSystem();
+            if (!inputSystem)
+            {
+                PyErr_SetString(
+                    PyExc_RuntimeError,
+                    "The Anvil input system is unavailable.");
+                return nullptr;
+            }
+
+            return inputSystem;
+        }
+    }
+
     PyObject* IsKeyPressed(PyObject*, PyObject* args)
     {
         int key = 0;
         if (!PyArg_ParseTuple(args, "i", &key))
             return nullptr;
 
-        App* app = App::GetInstance();
-        if (!app)
-        {
-            PyErr_SetString(
-                PyExc_RuntimeError,
-                "The Anvil application instance is unavailable.");
-            return nullptr;
-        }
-
-        auto inputSystem = app->GetInputSystem();
+        auto inputSystem = ResolveInputSystem();
         if (!inputSystem)
-        {
-            PyErr_SetString(
-                PyExc_RuntimeError,
-                "The Anvil input system is unavailable.");
             return nullptr;
-        }
 
         return PyBool_FromLong(inputSystem->IsKeyPressed(key) ? 1 : 0);
+    }
+
+    PyObject* IsKeyJustPressed(PyObject*, PyObject* args)
+    {
+        int key = 0;
+        if (!PyArg_ParseTuple(args, "i", &key))
+            return nullptr;
+
+        auto inputSystem = ResolveInputSystem();
+        if (!inputSystem)
+            return nullptr;
+
+        return PyBool_FromLong(inputSystem->IsKeyJustPressed(key) ? 1 : 0);
     }
 }
 #endif
