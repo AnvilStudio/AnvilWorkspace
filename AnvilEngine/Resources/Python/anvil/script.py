9ae45f3057759eb5db853aed7c12c983807ba710
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-08 23:26:14 -0400

    Share entity component API with scripts

--- AnvilEngine/Resources/Python/anvil/script.py
+++ AnvilEngine/Resources/Python/anvil/script.py
@@ -1,49 +1,9 @@
-import _anvil
-
-from .transform import Transform2D
-from .physics import RigidBody2D
-from .collider import BoxCollider2D
+from .entity import Entity
 
 
-class Script:
+class Script(Entity):
     def __init__(self):
-        self.entity_id = ""
-
-    def has_component(self, component_type) -> bool:
-        component_name = getattr(component_type, "_component_name", None)
-        if component_name is None:
-            raise TypeError("component_type must be an Anvil component type")
-
-        return bool(
-            _anvil._has_component(
-                self.entity_id,
-                component_name
-            )
-        )
-
-    def get_component(self, component_type):
-        component_name = getattr(component_type, "_component_name", None)
-        if component_name is None:
-            raise TypeError("component_type must be an Anvil component type")
-
-        if not self.has_component(component_type):
-            raise KeyError(
-                f"Entity does not have component {component_name}"
-            )
-
-        return component_type(self.entity_id)
-
-    @property
-    def transform(self):
-        return self.get_component(Transform2D)
-
-    @property
-    def rigid_body(self):
-        return self.get_component(RigidBody2D)
-
-    @property
-    def box_collider(self):
-        return self.get_component(BoxCollider2D)
+        super().__init__("")
 
     # Compatibility aliases. Prefer self.transform.position/rotation.
     @property
