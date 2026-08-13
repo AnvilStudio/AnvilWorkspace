6c5cf6e9e619e936675e3da3727f2622d84906e4
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-12 23:10:11 -0400

    Sample sprite textures in Vulkan shader

--- ProjectCreator/Template/Assets/com.anvstu.engine/ShaderLib/sprite.glsl
+++ ProjectCreator/Template/Assets/com.anvstu.engine/ShaderLib/sprite.glsl
@@ -16,9 +16,10 @@ layout(set = 0, binding = 0) uniform Camera
 } camera;
 
 layout(location = 0) in vec2 inPosition;
-layout(location = 1) in vec4 inColor;
+layout(location = 1) in vec2 inTexCoord;
 
 layout(location = 0) out vec4 fragColor;
+layout(location = 1) out vec2 fragTexCoord;
 
 void main()
 {
@@ -27,15 +28,19 @@ void main()
         vec4(inPosition, 0.0, 1.0);
 
     fragColor = push_data.color;
+    fragTexCoord = inTexCoord;
 }
 
 #type frag
 #version 450
 
-layout(location = 0) out vec4 outColor;
+layout(set = 1, binding = 0) uniform sampler2D spriteTexture;
+
 layout(location = 0) in vec4 fragColor;
+layout(location = 1) in vec2 fragTexCoord;
+layout(location = 0) out vec4 outColor;
 
 void main()
 {
-    outColor = fragColor;
+    outColor = texture(spriteTexture, fragTexCoord) * fragColor;
 }
