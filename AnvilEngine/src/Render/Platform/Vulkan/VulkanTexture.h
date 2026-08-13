0d9a54c099b32c79f3630152ae993c1d2d405dcc
Cayden Jordan <seawardspy1531@gmail.com>
2026-08-12 23:08:19 -0400

    Implement Vulkan texture resource ownership

--- AnvilEngine/src/Render/Platform/Vulkan/VulkanTexture.h
+++ AnvilEngine/src/Render/Platform/Vulkan/VulkanTexture.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Asset/AssetTypes/Texture.h>
+#include <Render/Context.h>
+#include <vulkan/vulkan.h>
 
 namespace anv
 {
@@ -9,16 +11,36 @@ namespace anv
     public:
         explicit VulkanTexture(const std::filesystem::path& path);
         explicit VulkanTexture(Deserialized& deserialized);
+        VulkanTexture(const unsigned char* rgbaPixels, int width, int height, const std::string& name);
         ~VulkanTexture() override;
 
         bool IsGPUReady() const override;
         void* GetNativeHandle() const override;
 
+        VkImage GetImage() const { return m_Image; }
+        VkImageView GetImageView() const { return m_ImageView; }
+        VkSampler GetSampler() const { return m_Sampler; }
+        VkDescriptorSet GetDescriptorSet() const { return m_DescriptorSet; }
+        VkDescriptorSetLayout GetDescriptorSetLayout() const { return m_DescriptorSetLayout; }
+
     private:
         void Upload();
         void Destroy();
+        void create_image();
+        void create_image_view();
+        void create_sampler();
+        void create_descriptor();
 
     private:
-        void* m_Image = nullptr;
+        _shared<Context> m_Context = nullptr;
+
+        VkImage m_Image = VK_NULL_HANDLE;
+        VkDeviceMemory m_Memory = VK_NULL_HANDLE;
+        VkImageView m_ImageView = VK_NULL_HANDLE;
+        VkSampler m_Sampler = VK_NULL_HANDLE;
+
+        VkDescriptorSetLayout m_DescriptorSetLayout = VK_NULL_HANDLE;
+        VkDescriptorPool m_DescriptorPool = VK_NULL_HANDLE;
+        VkDescriptorSet m_DescriptorSet = VK_NULL_HANDLE;
     };
 }
